452d3696c3a8ff325fd4fe7f057feb32ab153a07
AadiPawar602 <126225160+AadiPawar602@users.noreply.github.com>
2025-04-01 20:00:59 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatefood:06022002@cluster0.yyese.mongodb.net/').then (() =>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://<your-db>:<your-password>@cluster0.yyese.mongodb.net/').then (() =>console.log("DB Connected"));
 }
